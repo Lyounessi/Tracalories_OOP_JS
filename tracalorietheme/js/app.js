96b540ba6472e76001b414954ce341d620e1576c
boukroun.younes <boukroun.younes@gmail.com>
2025-11-10 00:34:17 +0100

    methods to update tracker info based on actions

--- tracalorietheme/js/app.js
+++ tracalorietheme/js/app.js
@@ -1,25 +1,51 @@
 class CalorieTracker {
     constructor() {
-        this._calorieLimit = 2000;
+        this._calorieLimit = 2800;
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        this._displayCaloriesLimit();
+        this._displayCaloriesTotal();
     }
-
+    // Public methods/API
     addMeal(meal) {
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        this._render();
     }
 
     addWorkout(workout) {
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        this._render();
     }
 
-    get totalCalories() {
-        return this._totalCalories;
+    // Private methods //
+
+    _displayCaloriesTotal  () {
+        const totalCaloriesEl = document.getElementById('calories-total');
+        totalCaloriesEl.innerHTML = this._totalCalories;
     }
-    
+
+    _displayCaloriesLimit() {
+        const calorieLimitEl = document.getElementById('calories-limit');
+        calorieLimitEl.innerHTML = this._calorieLimit;
+    }
+    _displayCaloriesConsumed() {
+        const caloriesConsumedEl = document.getElementById('calories-consumed');
+        const consumer = this._meals.reduce((total, meal) => total + meal.calories, 0);
+        caloriesConsumedEl.innerHTML = consumer;
+    }
+    _displayCaloriesBurned() {
+        const caloriesBurnedEl = document.getElementById('calories-burned');
+        const burned = this._workouts.reduce((total, workout) => total + workout.calories, 0);
+        caloriesBurnedEl.innerHTML = burned;
+    }
+    _render() {
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+    }
 }
 
 class Meal {
@@ -39,9 +65,10 @@ class Workout {
 }
 
 const calorieTracker = new CalorieTracker();
-const meal = new Meal("Salad", 100);
+const meal = new Meal("Salad", 600);
+const meal2 = new Meal("Pizza", 1000);
+const run = new Workout("Running", 340);
+const bike = new Workout("Biking", 200);
 calorieTracker.addMeal(meal);
-console.log(calorieTracker.totalCalories);
-const run = new Workout("Running", 100);
+calorieTracker.addMeal(meal2);
 calorieTracker.addWorkout(run);
-console.log(calorieTracker.totalCalories);
